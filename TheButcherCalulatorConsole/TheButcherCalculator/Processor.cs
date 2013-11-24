377e8a749386ab137542db1884c9583a85691e1c
dpenkova@gmail.com <dpenkova@gmail.com@1af1addb-3e62-4e4e-57a9-2b26a704a832>
2013-11-24 18:22:48 +0000

    Added a Processor class

---
+++ TheButcherCalulatorConsole/TheButcherCalculator/Processor.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheButcherCalculator
+{
+    public class Processor
+    {
+        public List<ISeparateable> InputList { get; set; }
+        public List<Product> GoodsProduced { get; set; }
+        public List<Product> WasteProduced { get; set; }
+
+        public Processor()
+        {
+            this.InputList = new List<ISeparateable>();
+            this.GoodsProduced = new List<Product>();
+            this.WasteProduced = new List<Product>();
+        }
+
+        public void Calculate()
+        {
+            this.GoodsProduced.Clear();
+            this.WasteProduced.Clear();
+
+            foreach (var item in this.InputList)
+            {
+                this.GoodsProduced.AddRange(item.ProduceGoods());
+                this.WasteProduced.AddRange(item.ProduceWaste());
+            }
+
+            //List<Product> tempGoods = SumProducts(GoodsProduced);
+            //this.GoodsProduced.Clear();
+            //this.GoodsProduced = tempGoods;
+
+            this.GoodsProduced = SumProducts(GoodsProduced);
+            this.WasteProduced = SumProducts(WasteProduced);
+
+
+        }
+
+        public List<Product> SumProducts(List<Product> input)
+        {
+            List<Product> sumResult = new List<Product>();
+            sumResult = input
+               .GroupBy(product => product.Name.ToString())
+               .Select(i => new Product()
+               {
+                   Name = i.Key,
+                   ProductWeight = i.Sum(x => x.ProductWeight)
+               })
+               .ToList();
+            return sumResult;
+        }
+
+        public void ClearLists()
+        {
+            this.InputList.Clear();
+            this.GoodsProduced.Clear();
+            this.WasteProduced.Clear();
+        }
+    }
+}
