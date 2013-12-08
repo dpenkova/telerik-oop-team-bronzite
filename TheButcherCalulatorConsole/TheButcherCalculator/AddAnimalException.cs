e82886723021d7e95f3a8d50cc23716f0e8b7cee
vassilpk@gmail.com <vassilpk@gmail.com@1af1addb-3e62-4e4e-57a9-2b26a704a832>
2013-12-08 15:10:11 +0000

    Some little changes and Exception class added

---
+++ TheButcherCalulatorConsole/TheButcherCalculator/AddAnimalException.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheButcherCalculator
+{
+    public class AddAnimalException : Exception
+    {
+        private string animalClassAndKind;
+
+        public string AnimalClassAndKind
+        {
+            get
+            {
+                return animalClassAndKind;
+            }
+        }
+        public AddAnimalException(string message, Exception causeException = null)
+            : this(message, null, causeException)
+        {
+        }
+
+        public AddAnimalException(string message, string animalClassAndKind,
+            Exception causeException = null)
+            : base(message, causeException)
+        {
+            this.animalClassAndKind = animalClassAndKind;
+        }
+    }
+}
--- TheButcherCalulatorConsole/TheButcherCalculator/Processor.cs
+++ TheButcherCalulatorConsole/TheButcherCalculator/Processor.cs
@@ -6,7 +6,6 @@ using System.Threading.Tasks;
 
 namespace TheButcherCalculator
 {
-    //test 2
     public class Processor
     {
         public List<ISeparateable> InputList { get; set; }
@@ -20,6 +19,50 @@ namespace TheButcherCalculator
             this.WasteProduced = new List<Product>();
         }
 
+        public void Add(object animalClassAndKind, string textBoxWeightString)
+        {
+            string strAnimalClassAndKind;
+            double weightValue;
+            try
+            {
+                strAnimalClassAndKind = animalClassAndKind.ToString();
+            }
+            catch (NullReferenceException ex)
+            {
+                if (textBoxWeightString == string.Empty)
+                {
+                    throw new AddAnimalException("Please choose an animal and enter its weight !", ex);
+                }
+                else
+                {
+                    throw new AddAnimalException("Please choose an animal !", ex);
+                }
+            }
+
+            try
+            {
+                weightValue = double.Parse(textBoxWeightString);
+            }
+            catch (FormatException ex)
+            {
+                throw new AddAnimalException(String.Format("Please enter the {0}'s weight !", strAnimalClassAndKind.ToLower()), strAnimalClassAndKind, ex);
+            }
+
+            string nmespace = animalClassAndKind.GetType().Namespace;
+            object assembly = animalClassAndKind.GetType().Assembly;
+            string name = string.Format("{0}.{1}, {2}", nmespace, animalClassAndKind, assembly.ToString());
+
+            // Get the type contained in the name string
+            Type type = Type.GetType(name, true); // name should be a string of AssemblyQualifiedName
+
+            // Create an instance of that type
+            object instance = Activator.CreateInstance(type); // All animals should have an empty constructor to be created this way
+            (instance as Animal).TotalWeight = weightValue;
+            (instance as Animal).Kind = strAnimalClassAndKind;
+
+            InputList.Add(instance as ISeparateable);
+        }
+
         public void Calculate()
         {
             this.GoodsProduced.Clear();
@@ -31,15 +74,8 @@ namespace TheButcherCalculator
                 this.WasteProduced.AddRange(item.ProduceWaste());
             }
 
-            //List<Product> tempGoods = SumProducts(GoodsProduced);
-            //this.GoodsProduced.Clear();
-            //this.GoodsProduced = tempGoods;
-            // to be deleted
-
             this.GoodsProduced = SumProducts(GoodsProduced);
             this.WasteProduced = SumProducts(WasteProduced);
-
-
         }
 
         public List<Product> SumProducts(List<Product> input)
